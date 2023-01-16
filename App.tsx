738d9ffd662fda02e7591d79de20cc051c168579
AxelNie <axel.nie17@gmail.com>
2023-01-16 20:50:19 +0100

    added carousel

--- App.tsx
+++ App.tsx
@@ -11,6 +11,13 @@ const App: React.FC = () => {
         forceCapitalLetter={true}
         onChange={string => console.log('PASS', string)}
       />
+      <Carousel
+        imgUrls={[
+          'https://d3aux7tjp119y2.cloudfront.net/images/Visualiseringscenter_21-CMSTemplate.width-1650.jpg',
+          'https://www.prologis.se/sites/sweden/files/images/2022/05/norkoping_sweden_web.jpeg',
+          'https://www.christineabroad.com/images//2020/04/att-go%CC%88ra-i-norrko%CC%88ping.jpg',
+        ]}
+      />
     </View>
   );
 };
